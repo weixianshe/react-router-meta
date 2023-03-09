e7592e378af6e645986ad59c73c0eaf93cc10e7f
weitong <1758646906@qq.com>
2023-03-09 09:27:17 +0800

    fix:修改了导出文件的位置

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,3 @@
 import Permissions from "./Permission";
-export {
-  BrowserRouter as Router,
-  Outlet,
-  useRoutes,
-  NavLink,
-  Link,
-} from "react-router-dom";
+
 export default Permissions;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import Permissions from "./components";
+
 export { compRoutes } from "./utils";
+export {
+  BrowserRouter as Router,
+  Outlet,
+  useRoutes,
+  NavLink,
+  Link,
+} from "react-router-dom";
+export { Permissions };
